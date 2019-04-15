b6c6d6e9fab2acf1a0498ee9909f4dfde276fbe5
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-04-15 14:39:45 -0600

    feat: fixes SSR issues

--- src/index.js
+++ src/index.js
@@ -5,24 +5,27 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-const extendConf = function (api, conf) {
+const extendQuasarConf = function (api, conf) {
   // for brevity
   let boot = conf.boot
 
   // make sure qmarkdown boot file is registered
-  if (!boot.includes('~@quasar/quasar-app-extension-qmarkdown/src/boot/qmarkdown.js')) {
-    boot.push('~@quasar/quasar-app-extension-qmarkdown/src/boot/qmarkdown.js')
-    // make sure boot file transpiles
-    conf.build.transpileDependencies.push(/quasar-app-extension-qmarkdown[\\/]src[\\/]boot/)
+  const bootFile = '~@quasar/quasar-app-extension-qmarkdown/src/boot/qmarkdown.js'
+  if (!boot.includes(bootFile)) {
+    boot.push(bootFile)
     console.log(` App Extension (qmarkdown) Info: 'Adding qmarkdown boot reference to your quasar.conf.js'`)
   }
+
+  // make sure boot file transpiles
+  conf.build.transpileDependencies.push(/quasar-app-extension-qmarkdown[\\/]src/)
 
   // // for brevity
   let css = conf.css
 
   // make sure qmarkdown css goes through webpack to avoid ssr issues
-  if (!css.includes('~@quasar/quasar-app-extension-qmarkdown/src/component/markdown.styl')) {
-    css.push('~@quasar/quasar-app-extension-qmarkdown/src/component/markdown.styl')
+  const cssFile = '~@quasar/quasar-app-extension-qmarkdown/src/component/markdown.styl'
+  if (!css.includes(cssFile)) {
+    css.push(cssFile)
     console.log(` App Extension (qmarkdown) Info: 'Adding markdown.styl css reference to your quasar.conf.js'`)
   }
 }
@@ -33,6 +36,6 @@ module.exports = function (api, ctx) {
 
   // extend quasar.conf
   api.extendQuasarConf((conf) => {
-    extendConf(api, conf)
+    extendQuasarConf(api, conf)
   })
 }
