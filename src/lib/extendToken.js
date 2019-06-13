e8a73e4ad23f52196ff4d62822c8fe7b3bca07e7
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-06-13 08:22:48 -0600

    chore: fixing exports

--- src/lib/extendToken.js
+++ src/lib/extendToken.js
@@ -1,4 +1,4 @@
-export function extendToken (md) {
+export default function extendToken (md) {
   const defaultRender = md.renderer.rules.code_inline
 
   md.renderer.rules.code_inline = (tokens, idx, options, env, self) => {
@@ -8,5 +8,3 @@ export function extendToken (md) {
     return defaultRender(tokens, idx, options, env, self)
   }
 }
-
-export default extendToken
