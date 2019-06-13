ad45842af671f0e417eda721aad22fa8f0c100fc
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-06-13 08:30:47 -0600

    temporary code

---
+++ src/lib/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import extendBlockQuote from './extendBlockQuote'
+import extendContainers from './extendContainers'
+import extendHeading from './extendHeading'
+import extendImage from './extendImage'
+import extendLink from './extendLink'
+import extendTable from './extendTable'
+import extendToken from './extendToken'
+import getTagParts from './getTagParts'
+import slugify from './slugify'
+
+export {
+  extendBlockQuote,
+  extendContainers,
+  extendHeading,
+  extendImage,
+  extendLink,
+  extendTable,
+  extendToken,
+  getTagParts,
+  slugify
+}
+
+// module.exports = {
+//   extendBlockQuote,
+//   extendContainers,
+//   extendHeading,
+//   extendImage,
+//   extendLink,
+//   extendTable,
+//   extendToken,
+//   getTagParts,
+//   slugify
+// }
