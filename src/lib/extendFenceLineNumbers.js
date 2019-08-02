eaa8f522de135b676ac399e920c17f23cecf8d45
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-02 14:38:29 -0600

    feat(QMarkdown): now has line numbers for code blocks
    use property `no-line-numbers` to turn off
    This does not apply to the QMarkdown loaders

---
+++ src/lib/extendFenceLineNumbers.js
@@ -0,0 +1,26 @@
+export default function extendFenceLineNumbers (md) {
+  const fence = md.renderer.rules.fence
+  md.renderer.rules.fence = (...args) => {
+    const rawCode = fence(...args)
+    const code = rawCode.slice(
+      rawCode.indexOf('<code>') + 6,
+      rawCode.indexOf('</code>')
+    )
+
+    const lines = code.split('\n')
+    if (lines.length < 3) {
+      return rawCode
+    }
+
+    const lineNumbersCode = [...Array(lines.length)]
+      .map((line, index) => `<span class="q-markup--line-number">${index + 1}</span><br>`).join('')
+
+    const lineNumbersWrapperCode =
+      `<div class="q-markdown--line-numbers">${lineNumbersCode}</div><div class="q-markdown--code-wrapper">${rawCode}</div>`
+
+    const finalCode =
+      `<div class="q-markdown--line-numbers-wrapper">${lineNumbersWrapperCode}</div>`
+
+    return finalCode
+  }
+}
