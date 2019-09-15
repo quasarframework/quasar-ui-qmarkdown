34097c7a2f1a90d2d788866c8452ed311e720da7
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-09-15 10:16:04 -0600

    chore(demo): general improvements

--- demo/src/layouts/MarkdownLayout.vue
+++ demo/src/layouts/MarkdownLayout.vue
@@ -44,81 +44,7 @@
           icon="fas fa-link"
           label="Essential Links"
         >
-          <q-list>
-            <q-item clickable tag="a" target="_blank" href="http://quasar.dev">
-              <q-item-section avatar>
-                <q-icon name="school"></q-icon>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Docs</q-item-label>
-                <q-item-label caption>quasar.dev</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-              <q-item-section avatar>
-                <q-icon name="code"></q-icon>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Github</q-item-label>
-                <q-item-label caption>github.com/quasarframework</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable tag="a" target="_blank" href="http://chat.quasar.dev">
-              <q-item-section avatar>
-                <q-icon name="chat"></q-icon>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Discord Chat Channel</q-item-label>
-                <q-item-label caption>chat.quasar.dev</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-              <q-item-section avatar>
-                <q-icon name="record_voice_over"></q-icon>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Forum</q-item-label>
-                <q-item-label caption>forum.quasar.dev</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-              <q-item-section avatar>
-                <q-icon name="rss_feed"></q-icon>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Twitter</q-item-label>
-                <q-item-label caption>@quasarframework</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable to="/docs">
-              <q-item-section avatar>
-                <q-icon name="calendar_today" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>QMarkdown docs</q-item-label>
-                <q-item-label caption>Documentation</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable to="/demo">
-              <q-item-section avatar>
-                <q-icon name="calendar_today" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>QMarkdown demo</q-item-label>
-                <q-item-label caption>Interactively play with properties</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable to="/examples">
-              <q-item-section avatar>
-                <q-icon name="calendar_today" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>QMarkdown examples</q-item-label>
-                <q-item-label caption>Examples of how to do it</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
+        <essential-links />
         </q-expansion-item>
     </q-drawer>
 
@@ -136,7 +62,8 @@ import { version } from '@quasar/quasar-app-extension-qmarkdown/package.json'
 export default {
   name: 'MarkdownLayout',
   components: {
-    MarkdownTypes
+    MarkdownTypes,
+    'essential-links': () => import('../components/EssentialLinks')
   },
   data () {
     return {
