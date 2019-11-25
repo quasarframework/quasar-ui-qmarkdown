19b5807cde5d3a23cbf89d707a34bdabc80c18b6
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-11-25 09:09:51 -0700

    feat: add support for dark mode (#43)

--- demo/src/components/Hero.vue
+++ demo/src/components/Hero.vue
@@ -14,7 +14,7 @@
     </section>
     <main class="flex flex-start justify-center inset-shadow">
       <div class="q-pa-md col-12-sm col-8-md col-6-lg inset-shadow" style="width: 100%; height: 3px;" />
-      <div class="q-pa-md col-12-sm col-8-md col-6-lg bg-white shadow-1" style="max-width: 800px; width: 100%;">
+      <div class="q-pa-md col-12-sm col-8-md col-6-lg shadow-1" style="max-width: 800px; width: 100%;">
         <slot></slot>
       </div>
     </main>
