557d485c6ea57500a063fb061248412ef55ef622
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-17 12:04:25 -0700

    chore: add examples to dev

---
+++ ui/dev/src/pages/Code.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-toggle v-model="model" label="Disable highlighting"></q-toggle>
+    <q-toggle v-model="model2" label="Disable line numbers"></q-toggle>
+    <q-markdown
+      :no-highlight="model"
+      :no-line-numbers="model2"
+    >
+    Indented code
+
+Inline `code`
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+Block code "fences"
+
+```
+Sample text here...
+```
+
+Syntax highlighting
+
+```js
+var foo = function (bar) {
+  return bar++;
+};
+
+console.log(foo(5));
+```
+    </q-markdown>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: false,
+      model2: false
+    }
+  }
+}
+</script>
---
+++ ui/dev/src/pages/Images.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-toggle v-model="model" label="Disable images"></q-toggle>
+    <q-toggle v-model="model2" label="Disable links"></q-toggle>
+    <q-toggle v-model="model3" label="Disable linkify"></q-toggle>
+    <q-markdown
+      :no-image="model"
+      :no-link="model2"
+      :no-linkify="model3"
+    >
+Max fixed size image: 200x200; responsive
+![Minion](https://octodex.github.com/images/minion.png =200x200)
+
+Max fixed size image: 300x300; with title; responsive
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat" =300x300)
+
+Responsive image - adjust browser to see auto-resize
+![Minion](https://octodex.github.com/images/minion.png "Responsive Image")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location (see code for markdown):
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+    </q-markdown>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: false,
+      model2: false,
+      model3: false
+    }
+  }
+}
+</script>
