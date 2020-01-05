b3b6b14aaf0a40143c274f1f15f7599755b3bf06
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-01-05 11:19:51 -0700

    chore: update deps

--- ui/build/script.javascript.js
+++ ui/build/script.javascript.js
@@ -5,11 +5,15 @@ const rollup = require('rollup')
 const uglify = require('uglify-es')
 const buble = require('@rollup/plugin-buble')
 const json = require('@rollup/plugin-json')
-const cjs = require('rollup-plugin-commonjs')
-const nodeResolve = require('rollup-plugin-node-resolve')
+const cjs = require('@rollup/plugin-commonjs')
+const nodeResolve = require('@rollup/plugin-node-resolve')
 
 const buildConf = require('./config')
 const buildUtils = require('./utils')
+
+const bubleConfig = {
+  objectAssign: 'Object.assign'
+}
 
 const rollupPlugins = [
   nodeResolve({
@@ -18,9 +22,6 @@ const rollupPlugins = [
   }),
   json(),
   cjs()
-  // buble({
-  //   objectAssign: 'Object.assign'
-  // })
 ]
 
 const builds = [
