289c75de35e78880f1865b5959ab0b97817e3a4f
Jeff Galbraith <galbraith64@gmail.com>
2021-06-14 08:52:34 -0600

    fix(build): complaint from webpack

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,4 +1,5 @@
-import { version } from '../package.json'
+import pkg from '../package.json'
+const { version } = pkg
 import QMarkdown from './components/QMarkdown'
 import getTagParts from './util/getTagParts'
 
