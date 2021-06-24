c7c9168f6a8b8e6ca93394a56b5eddf0b1e28944
Jeff Galbraith <galbraith64@gmail.com>
2021-06-24 09:18:39 -0600

    chore: eslint fixes

--- ui/build/script.javascript.js
+++ ui/build/script.javascript.js
@@ -118,24 +118,24 @@ function addAssets (builds, type, injectName) {
   const
     files = fs.readdirSync(pathResolve('../../ui/src/components/' + type)),
     plugins = [buble(bubleConfig)],
-    outputDir = pathResolve(`../dist/${type}`)
+    outputDir = pathResolve(`../dist/${ type }`)
 
   fse.mkdirp(outputDir)
 
   files
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
-      const name = file.substr(0, file.length - 3).replace(/-([a-z])/g, g => g[1].toUpperCase())
+      const name = file.substr(0, file.length - 3).replace(/-([a-z])/g, g => g[ 1 ].toUpperCase())
       builds.push({
         rollup: {
           input: {
-            input: pathResolve(`../src/components/${type}/${file}`),
+            input: pathResolve(`../src/components/${ type }/${ file }`),
             plugins
           },
           output: {
-            file: addExtension(pathResolve(`../dist/${type}/${file}`), 'umd'),
+            file: addExtension(pathResolve(`../dist/${ type }/${ file }`), 'umd'),
             format: 'umd',
-            name: `QMarkdown.${injectName}.${name}`
+            name: `QMarkdown.${ injectName }.${ name }`
           }
         },
         build: {
@@ -154,7 +154,7 @@ function build (builds) {
 function genConfig (opts) {
   Object.assign(opts.rollup.input, {
     plugins: rollupPlugins,
-    external: ['vue', 'quasar']
+    external: [ 'vue', 'quasar' ]
   })
 
   Object.assign(opts.rollup.output, {
@@ -167,7 +167,7 @@ function genConfig (opts) {
 
 function addExtension (filename, ext = 'min') {
   const insertionPoint = filename.lastIndexOf('.')
-  return `${filename.slice(0, insertionPoint)}.${ext}${filename.slice(insertionPoint)}`
+  return `${ filename.slice(0, insertionPoint) }.${ ext }${ filename.slice(insertionPoint) }`
 }
 
 function buildEntry (config) {
@@ -176,8 +176,8 @@ function buildEntry (config) {
     .then(bundle => bundle.generate(config.rollup.output))
     .then(({ output }) => {
       const code = config.rollup.output.format === 'umd'
-        ? injectVueRequirement(output[0].code)
-        : output[0].code
+        ? injectVueRequirement(output[ 0 ].code)
+        : output[ 0 ].code
 
       return config.build.unminified
         ? buildUtils.writeFile(config.rollup.output.file, code)
@@ -226,7 +226,7 @@ function injectVueRequirement (code) {
   }
   `
 
-  return code.substring(0, index - 1) +
-    checkMe +
-    code.substring(index)
+  return code.substring(0, index - 1)
+    + checkMe
+    + code.substring(index)
 }
