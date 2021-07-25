7a10c0862039f524397ccd2281e0bd91e5658d35
Jeff Galbraith <galbraith64@gmail.com>
2021-07-25 13:37:29 -0600

    chore: code clean up

--- app-extension/src/boot/register.js
+++ app-extension/src/boot/register.js
@@ -1,6 +1,5 @@
 import { boot } from 'quasar/wrappers'
 import VuePlugin from '@quasar/quasar-ui-qmarkdown/src/index.js'
-import '@quasar/quasar-ui-qmarkdown/dist/index.css'
 
 export default boot(({ app }) => {
   app.use(VuePlugin)
