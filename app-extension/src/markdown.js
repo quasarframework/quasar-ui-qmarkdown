78be13cc9b52e6152fbd4f6495eea91a16022220
Jeff Galbraith <galbraith64@gmail.com>
2021-09-19 11:03:53 -0600

    chore: eslint fixes

--- app-extension/src/markdown.js
+++ app-extension/src/markdown.js
@@ -19,11 +19,11 @@ function removeFrontMatter (source) {
 }
 
 function replaceFrontMatter (source, content) {
-  return String(source).replace('frontMatter: {}', `frontMatter: ${JSON.stringify(content)}`)
+  return String(source).replace('frontMatter: {}', `frontMatter: ${ JSON.stringify(content) }`)
 }
 
 function replaceToc (source, tocData) {
-  return String(source).replace('tocData: []', `tocData: ${JSON.stringify(tocData)}`)
+  return String(source).replace('tocData: []', `tocData: ${ JSON.stringify(tocData) }`)
 }
 
 function slugify (str) {
@@ -33,15 +33,16 @@ function slugify (str) {
 function highlight (str, lang) {
   if (lang === '') {
     lang = 'js'
-  } else if (lang === 'vue' || lang === 'html') {
+  }
+  else if (lang === 'vue' || lang === 'html') {
     lang = 'html'
   }
 
-  if (Prism.languages[lang] !== undefined) {
-    const code = Prism.highlight(str, Prism.languages[lang], lang)
+  if (Prism.languages[ lang ] !== undefined) {
+    const code = Prism.highlight(str, Prism.languages[ lang ], lang)
 
-    return '<pre class="q-markdown--code">' +
-      `<code class="q-markdown--code__inner language-${lang}">${code}</code></pre>\n`
+    return '<pre class="q-markdown--code">'
+    + `<code class="q-markdown--code__inner language-${ lang }">${ code }</code></pre>\n`
   }
 
   return ''
@@ -51,7 +52,7 @@ function extendToken (md) {
   const defaultRender = md.renderer.rules.code_inline
 
   md.renderer.rules.code_inline = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
     token.attrSet('class', 'q-markdown--token')
     return defaultRender(tokens, idx, options, env, self)
@@ -60,7 +61,7 @@ function extendToken (md) {
 
 function extendTable (md) {
   md.renderer.rules.table_open = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
     token.attrSet('class', 'q-markdown--table')
 
@@ -70,25 +71,27 @@ function extendTable (md) {
 
 function extendLink (md) {
   md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
     const hrefIndex = token.attrIndex('href')
 
-    if (token.attrs[hrefIndex][1][0] === '#') {
+    if (token.attrs[ hrefIndex ][ 1 ][ 0 ] === '#') {
       if (location) {
-        token.attrs[hrefIndex][1] = location.pathname + token.attrs[hrefIndex][1]
+        token.attrs[ hrefIndex ][ 1 ] = location.pathname + token.attrs[ hrefIndex ][ 1 ]
       }
     }
 
-    if (token.attrs[hrefIndex][1] === '') {
+    if (token.attrs[ hrefIndex ][ 1 ] === '') {
       token.attrSet('class', 'q-markdown--link q-markdown--link-local')
-      if (tokens[idx + 1] && tokens[idx + 1].type === 'text' && tokens[idx + 1].content) {
-        token.attrSet('id', slugify(tokens[idx + 1].content))
+      if (tokens[ idx + 1 ] && tokens[ idx + 1 ].type === 'text' && tokens[ idx + 1 ].content) {
+        token.attrSet('id', slugify(tokens[ idx + 1 ].content))
       }
-    } else if (token.attrs[hrefIndex][1][0] === '/' ||
-      token.attrs[hrefIndex][1].startsWith('..')) {
+    }
+    else if (token.attrs[ hrefIndex ][ 1 ][ 0 ] === '/'
+      || token.attrs[ hrefIndex ][ 1 ].startsWith('..')) {
       token.attrSet('class', 'q-markdown--link q-markdown--link-local')
-    } else {
+    }
+ else {
       token.attrSet('class', 'q-markdown--link q-markdown--link-external')
       token.attrSet('target', '_blank')
     }
@@ -99,7 +102,7 @@ function extendLink (md) {
 
 function extendImage (md) {
   md.renderer.rules.image = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
     token.attrSet('class', 'q-markdown--image')
     return self.renderToken(tokens, idx, options)
@@ -108,17 +111,18 @@ function extendImage (md) {
 
 function extendHeading (md, tocData = [], toc = false, tocStart = 1, tocEnd = 3) {
   md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
-    const label = tokens[idx + 1]
+    const label = tokens[ idx + 1 ]
       .children
       .reduce((acc, t) => acc + t.content, '')
 
-    let classes = `q-markdown--heading-${token.tag}`
+    let classes = `q-markdown--heading-${ token.tag }`
 
     if (token.markup === '=') {
       classes += ' q-markdown--title-heavy'
-    } else if (token.markup === '-') {
+    }
+ else if (token.markup === '-') {
       classes += ' q-markdown--title-light'
     }
 
@@ -128,7 +132,7 @@ function extendHeading (md, tocData = [], toc = false, tocStart = 1, tocEnd = 3)
     token.attrSet('class', classes)
 
     if (toc) {
-      const tokenNumber = parseInt(token.tag[1])
+      const tokenNumber = parseInt(token.tag[ 1 ])
 
       if (tocStart && tocEnd && tocStart < tocEnd && tokenNumber >= tocStart && tokenNumber <= tocEnd) {
         tocData.push({ id: id, label: label, level: tokenNumber, children: [] })
@@ -145,11 +149,12 @@ function createContainer (className, defaultTitle) {
     className,
     {
       render (tokens, idx) {
-        const token = tokens[idx]
+        const token = tokens[ idx ]
         const info = token.info.trim().slice(className.length).trim()
         if (token.nesting === 1) {
-          return `<div class="q-markdown--note q-markdown--note--${className}"><p class="q-markdown--note-title">${info || defaultTitle}</p>\n`
-        } else {
+          return `<div class="q-markdown--note q-markdown--note--${ className }"><p class="q-markdown--note-title">${ info || defaultTitle }</p>\n`
+        }
+ else {
           return '</div>\n'
         }
       }
@@ -166,15 +171,15 @@ function extendContainers (md) {
 
   // explicitly escape Vue syntax
   md.use(container, 'v-pre', {
-    render: (tokens, idx) => tokens[idx].nesting === 1
+    render: (tokens, idx) => (tokens[ idx ].nesting === 1
       ? '<div v-pre>\n'
-      : '</div>\n'
+      : '</div>\n')
   })
 }
 
 function extendBlockQuote (md) {
   md.renderer.rules.blockquote_open = (tokens, idx, options, env, self) => {
-    const token = tokens[idx]
+    const token = tokens[ idx ]
 
     token.attrSet('class', 'q-markdown--note')
     return self.renderToken(tokens, idx, options)
