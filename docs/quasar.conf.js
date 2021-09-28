7a874ef147e010b18f85639f2e3499daeafcdd5c
Jeff Galbraith <galbraith64@gmail.com>
2021-09-28 09:31:52 -0600

    fix(docs): a few docs issues get fixed

--- docs/quasar.conf.js
+++ docs/quasar.conf.js
@@ -28,8 +28,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.sass',
-      'Markdown.sass'
+      'app.sass'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
--- docs/src/pages/latest-news/changelog.md
+++ docs/src/pages/latest-news/changelog.md
@@ -3,6 +3,6 @@ title: Changelog
 desc: The latest QMarkdown releases
 keys: latest-news
 components:
-- releases/Releases
+  - releases/Releases
 ---
 <releases class="q-mt-sm"/>
