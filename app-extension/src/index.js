008a07fadbd89c29facbf8cd62c9fee8e92e6e3f
Jeff Galbraith <galbraith64@gmail.com>
2022-03-19 12:45:27 -0600

    feat(app-ext): support for app-webpack and app-vite

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -6,44 +6,78 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-const renderMarkdown = require('./markdown')
-
-function extendConf (conf) {
-  // register our boot file
-  conf.boot.push('~@quasar/quasar-app-extension-qmarkdown/src/boot/register.js')
-
-  // make sure app extension files & ui packages get transpiled
-  conf.build.transpileDependencies.push(/quasar-app-extension-qmarkdown[\\/]src/)
-  conf.build.transpileDependencies.push(/quasar-ui-qmarkdown[\\/]src/)
-
-  // make sure these plugins are in the build
-  conf.framework.plugins.push('Dialog')
-
-  // This needs to be set for Vue 3
-  const oldPreTagFunc = conf.build.vueLoaderOptions.compilerOptions.isPreTag
-  conf.build.vueLoaderOptions.compilerOptions.isPreTag = (tag) => tag === 'pre' || tag === 'q-markdown' || (typeof oldPreTagFunc === 'function' ? oldPreTagFunc(tag) : false)
-
-  // make sure the stylesheet goes through webpack to avoid SSR issues
-  conf.css.push('~@quasar/quasar-ui-qmarkdown/src/index.sass')
-}
-
-module.exports = function (api) {
-  // Quasar compatibility check; you may need
-  // hard dependencies, as in a minimum version of the "quasar"
-  // package or a minimum version of "@quasar/app" CLI
-  api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app', '^3.0.0')
-
-  // Uncomment the line below if you provide a JSON API for your component
-  api.registerDescribeApi('QMarkdown', '~@quasar/quasar-ui-qmarkdown/dist/api/QMarkdown.json')
-
-  // We extend /quasar.conf.js
-  api.extendQuasarConf(extendConf)
-
-  // chain webpack
-  api.chainWebpack((chain) => chainWebpack(api, chain))
-}
-
+ const renderMarkdown = require('./markdown')
+ const { merge } = require('webpack-merge')
+ 
+ function extendConf (conf, api) {
+   // register our boot file
+   conf.boot.push('~@quasar/quasar-app-extension-qmarkdown/src/boot/register.js')
+ 
+   if (api.hasVite !== true) {
+     // make sure app extension files & ui packages get transpiled
+     conf.build.transpileDependencies.push(/quasar-app-extension-qmarkdown[\\/]src/)
+     conf.build.transpileDependencies.push(/quasar-ui-qmarkdown[\\/]src/)
+   }
+ 
+   // make sure these plugins are in the build
+   conf.framework.plugins.push('Dialog')
+ 
+   let compilerOptions = null
+ 
+   // make sure to have 'compilerOptions.isPreTag' available
+   if (api.hasVite === true) {
+     conf.build = merge({
+       viteVuePluginOptions: {
+         template: {
+           compilerOptions: {
+             isPreTag: (tag) => tag === 'pre'
+           }
+         }
+       }
+     }, conf.build)
+     compilerOptions = conf.build.viteVuePluginOptions.template.compilerOptions
+   }
+   else {
+     conf.build = merge({
+       vueLoaderOptions: {
+         compilerOptions: {
+           isPreTag: (tag) => tag === 'pre'
+         }
+       }
+     }, conf.build)
+     compilerOptions = conf.build.vueLoaderOptions.compilerOptions
+   }
+ 
+   // This needs to be set for Vue 3
+   const oldPreTagFunc = compilerOptions.isPreTag
+   compilerOptions.isPreTag = (tag) => tag === 'pre' || tag === 'q-markdown' || (typeof oldPreTagFunc === 'function' ? oldPreTagFunc(tag) : false)
+ 
+ 
+   // make sure the stylesheet goes through webpack to avoid SSR issues
+   conf.css.push('~@quasar/quasar-ui-qmarkdown/src/index.sass')
+ }
+ 
+ module.exports = function (api) {
+   // Quasar compatibility check; you may need
+   // hard dependencies, as in a minimum version of the "quasar"
+   // package or a minimum version of "@quasar/app" CLI
+   api.compatibleWith('quasar', '^2.0.0')
+ 
+   if (api.hasVite === true) {
+     api.compatibleWith('@quasar/app-vite', '^1.0.0-alpha.0')
+   }
+   else {
+     // should be "@quasar/app-webpack" but that is not backward compatible
+     api.compatibleWith('@quasar/app', '^3.0.0')
+   }
+ 
+   // Uncomment the line below if you provide a JSON API for your component
+   api.registerDescribeApi('QMarkdown', '~@quasar/quasar-ui-qmarkdown/dist/api/QMarkdown.json')
+ 
+   // We extend /quasar.conf.js
+   api.extendQuasarConf(extendConf)
+ }
+ 
 const chainWebpack = function (api, chain) {
   if (api.prompts.import_md !== undefined && api.prompts.import_md === true) {
     console.log(' App Extension (qmarkdown) Info: \'Adding markdown loader to chainWebpack\'')
