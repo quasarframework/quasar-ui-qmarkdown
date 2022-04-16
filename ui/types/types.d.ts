96ef812e946117c28a2ec203d04d2da6d5deeaf7
Jeff Galbraith <galbraith64@gmail.com>
2022-04-16 11:46:38 -0600

    feat(ui): improved TS definitions for MarkdownItPluginsArray

--- ui/types/types.d.ts
+++ ui/types/types.d.ts
@@ -1,10 +1,13 @@
+import { MarkdownIt } from "@types/markdown-it";
 export * from "./vue-prop-types";
+
 export interface TocDefinition {
     id: String,
     label: string,
     level?: number,
     children?: TocDefinitionArray
-  }
-  
-export type TocDefinitionArray = TocDefinition[]
-  
+  };
+export type TocDefinitionArray = TocDefinition[];
+
+export interface MarkdownItPlugin = MarkdownIt.PluginSimple | MarkdownIt.PluginWithOptions | MarkdownIt.PluginWithParams;
+export type MarkdownItPluginsArray = MarkdownItPlugin[];
