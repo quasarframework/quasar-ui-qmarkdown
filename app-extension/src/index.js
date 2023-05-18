0d9374185c3348de8cfd8c771a62398a4ceec113
Jeff <galbraith64@gmail.com>
2023-05-18 10:00:42 -0600

    feat(app-ext): Future compatible with next version of quasar/app-*

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -64,11 +64,10 @@ module.exports = function (api) {
   api.compatibleWith('quasar', '^2.0.0')
 
   if (api.hasVite === true) {
-    api.compatibleWith('@quasar/app-vite', '^1.0.0-alpha.0')
+    api.compatibleWith('@quasar/app-vite', '^1.0.0|^2.0.0')
   }
   else {
-    // should be "@quasar/app-webpack" but that is not backward compatible
-    api.compatibleWith('@quasar/app', '^3.0.0')
+    api.compatibleWith('@quasar/app-webpack', '^3.0.0|^4.0.0')
   }
 
   // Uncomment the line below if you provide a JSON API for your component
