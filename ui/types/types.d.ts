47c30413647672c2b551dd82c2eda034a20667f6
Jeff <galbraith64@gmail.com>
2024-12-18 11:29:16 -0700

    fix(ui): Error / warning when importing useQMarkdownGlobalProps (#388)

--- ui/types/types.d.ts
+++ ui/types/types.d.ts
@@ -15,3 +15,22 @@ export type MarkdownItPlugin =
   | MarkdownIt.PluginWithOptions
   | MarkdownIt.PluginWithParams
 export type MarkdownItPluginsArray = MarkdownItPlugin[]
+
+interface QMarkdownGlobalProps {
+  lineNumberAlt?: string
+  noBlockquote?: boolean
+  noBreaks?: boolean
+  noContainer?: boolean
+  noHeadingAnchorLinks?: boolean
+  noHighlight?: boolean
+  noHtml?: boolean
+  noImage?: boolean
+  noLineNumbers?: boolean
+  noLink?: boolean
+  noLinkify?: boolean
+  noTypographer?: boolean
+}
+
+declare function useQMarkdownGlobalProps(props: QMarkdownGlobalProps): void
+
+export { useQMarkdownGlobalProps, QMarkdownGlobalProps }
