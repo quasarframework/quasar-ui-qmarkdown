035517a7721e5fbe6b769455288d90933ca79639
Jeff <galbraith64@gmail.com>
2026-06-05 06:54:46 -0600

    refine landing preview label

--- packages/docs/src/components/page-parts/LandingPageContent.vue
+++ packages/docs/src/components/page-parts/LandingPageContent.vue
@@ -76,7 +76,7 @@
           <div class="preview-panel">
             <div class="preview-panel__header">
               <span class="preview-panel__kicker">{{ previewKicker }}</span>
-              <span class="preview-panel__note">Docs and app-ready</span>
+              <span class="preview-panel__note">Markdown to interface</span>
             </div>
 
             <div class="preview-panel__body">
