f3fc6aeaedf0deda8b38e2c3580b756400466011
Jeff <galbraith64@gmail.com>
2026-06-15 11:15:54 -0600

    docs: refresh typography replacement example

--- packages/docs/src/examples/QMarkdown/Typography.vue
+++ packages/docs/src/examples/QMarkdown/Typography.vue
@@ -10,13 +10,24 @@ import { ref } from "vue";
 import { QMarkdown } from "@quasar/quasar-ui-qmarkdown";
 import "@quasar/quasar-ui-qmarkdown/dist/index.css";
 
-const markdown = `(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+const markdown = `Copyright and marks:
+(c) (C) (r) (R) (tm) (TM)
 
+Math:
++-
+
+Ellipses and punctuation cleanup:
 test.. test... test..... test?..... test!...
+!!!!!! ???? ,,
 
-!!!!!! ???? ,,  -- ---
+Dashes:
+word -- word
+word --- word
 
-Smartypants: "double quotes" and 'single quotes'`;
+Smartypants:
+"double quotes", 'single quotes', and it's John's book.
+
+Note: markdown-it 14 no longer replaces \`(p)\` or \`(P)\`. Use \`§\`, \`¶\`, or an HTML entity directly when you need those symbols.`;
 
 defineOptions({ name: "Typography" });
 
