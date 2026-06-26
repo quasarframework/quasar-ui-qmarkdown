61eb0d0a43bd56f2a5c29952c02053f9c6e909d5
Jeff <galbraith64@gmail.com>
2026-06-26 15:38:29 -0600

    chore: update qpress to 0.1.0-rc.17

--- packages/docs/src/siteConfig/index.ts
+++ packages/docs/src/siteConfig/index.ts
@@ -99,7 +99,6 @@ export interface SiteConfig {
   lang: string
   title: string
   description: string
-  theme: string
   version: string
   copyright: CopyrightConfig
   githubEditRootSrc: string
@@ -269,7 +268,6 @@ const config: SiteConfig = {
   lang: 'en-US',
   title: productName,
   description: 'Inline markdown for Vue and Quasar applications',
-  theme: 'doc',
   version,
   copyright: {
     line1: `Copyright © 2019-${new Date().getFullYear()} Jeff Galbraith`,
